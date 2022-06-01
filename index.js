7c17eae434250a951efc9e169b99f9ce2afbe140
marianamoiolicapelari <marianamoioli@gmail.com>
2022-06-01 10:31:42 -0300

    Added alt

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const express = require('express')
-const port = 3000
+/*
+  - Query params => meusite.com/users?nome=rodolfo&age=28 // FILTROS
+  - Route params => /users/2  // BUSCAR, DELETAR OU ARMAZENAR ALGO ESPECÍFICO
+  - Request Body => {"name":"Rodolfo", "age":28}
 
+  - GET          => Buscar informação no back-end
+  - POST         => Criar informação no back-end
+  - PUT / PATCH  => Alterar/Atualizar informação no back-end
+  - DELETE       => Deletar informação no back-end
+*/
+const express = require('express')
+const uuid = require('uuid')
+
+const port = 3000
 const app= express()
+app.use(express.json())
+
+const users = []
 
 app.get('/users', (request, response) =>{
-  return response.send('Hello Express')
+    return response.json(users)
 })
+
+app.post('/users', (request, response) =>{
+  const { name, age } = request.body
+
+  const user = { id:uuid.v4(), name, age }
+
+  users.push(user)
+
+  return response.status(201).json(user)
+})
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () =>{
   console.log(`🚀 Server started on port ${port}`)
